1d3061c47c56838eb1cc2db9f7152bb1fb4b53f5
Marcus <marcusrichardson7838@gmail.com>
2025-11-05 11:23:57 +0000

    Added a converter, didnt end up needing it but useful to have

--- src/config.py
+++ src/config.py
@@ -3,3 +3,7 @@ from pathlib import Path
 XML_FILE = (
     Path(__file__).parent.parent / "library_of_a_legend" / "library.xml"
 ).resolve()
+
+PLIST_INPUT_FILE = Path(__file__).parent.parent / "plist_file" / "preferences.plist"
+
+PLIST_OUTPUT_FILE = Path(__file__).parent.parent / "library_of_a_legend" / "Library.xml"
