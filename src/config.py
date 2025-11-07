21631dd5a06fcad64f70f03352983d8c86720ef3
blueninja804 <marcusrichardson7838@gmail.com>
2025-11-07 16:17:57 +0000

    need to finish skip count func

--- src/config.py
+++ src/config.py
@@ -3,4 +3,3 @@ from pathlib import Path
 XML_FILE = (
     Path(__file__).parent.parent / "library_of_a_legend" / "library.xml"
 ).resolve()
-
--- src/services.py
+++ src/services.py
@@ -43,6 +43,20 @@ class XmlReader:
         else:
             print(xml_file)
             return []
+        
+    def view_highest_skips(self) -> dict[str, int]:
+        artist_skip_count: dict[str, int] = {}
+        albums_skip_count: dict[str, int] = {}
+        xml_file = XmlReader.read_xml(self)
+        if isinstance(xml_file, dict):
+            tracks = cast(dict[str, dict[str, Any]], xml_file.get("Tracks", {}))
+            for _, v in tracks.items():
+                artist = v.get("Artist")
+                album = v.get("Album")
+                skip_count = v.get("Skip Count")
+                if artist:
+                    pass
+                    #come back to later
 
 
 if __name__ == "__main__":
